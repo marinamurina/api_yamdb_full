823259f87f57fbc484316b75329043710111eb96
Марина Мурина <marina_murina@bk.ru>
2022-10-09 19:54:36 +0300

    five

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+
 from reviews.models import User
 
 
@@ -23,16 +24,23 @@ class IsAdminModeratorOwnerOrReadOnly(permissions.BasePermission):
     администраторов, модераторов."""
 
     def has_permission(self, request, view):
-        return True
-        # return (request.method in permissions.SAFE_METHODS
-        #        or request.user.is_authenticated)
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
-        return True
-        # return (request.method in permissions.SAFE_METHODS
-        #         or request.user.is_admin
-        #         or request.user.is_moderator
-        #         or obj.author == request.user)
+        return (
+            request.method in permissions.SAFE_METHODS
+            or User.objects.get(pk=request.user).role == 'Admin'
+            or User.objects.get(pk=request.user).role == 'moderator'
+            or obj.author == request.user
+        )
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_admin
 
 
 class IsAdmin(permissions.BasePermission):
