7502e824b00efefa93bc5ce6a490d760e8510b6f
Марина Мурина <marina_murina@bk.ru>
2022-10-10 13:35:36 +0300

    seven again3

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -45,6 +45,7 @@ class IsAdminModeratorOwnerOrReadOnly(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
+
         return (
             request.method in permissions.SAFE_METHODS
             or request.user.is_admin
