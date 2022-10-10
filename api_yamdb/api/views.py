c8771c215228a702a78a5d4ef628f5ffe613eb92
Марина Мурина <marina_murina@bk.ru>
2022-10-10 15:18:03 +0300

    eight again

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -99,7 +99,8 @@ class ReviewViewSet(viewsets.ModelViewSet):
         title_id = self.kwargs.get('title_id')
         serializer.save(
             author=self.request.user,
-            title=title_id
+            title=get_object_or_404(Title, id=title_id)
+        
         )
 
 
