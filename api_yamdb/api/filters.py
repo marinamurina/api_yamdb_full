595a3bad44462aeb3ee76ccd6fb3e7121c0f6272
Чесноков Денис <d.chesnokov@pec.org.ru>
2022-10-10 15:22:17 +0300

    fix filters

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -4,12 +4,15 @@ from reviews.models import Title
 
 class TitleFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='name', lookup_expr='iexact')
+    year = django_filters.NumberFilter(field_name='year')
+    genre__slug = django_filters.CharFilter(lookup_expr='icontains')
+    category__slug = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta():
         model = Title
         fields = (
             'name',
-            # 'year',
-            # 'genre',
-            # 'category',
+            'year',
+            'genre',
+            'category',
         )
