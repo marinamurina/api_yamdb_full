dd08e031025ca13c2f2d16ec039ac5662501a42f
Чесноков Денис <d.chesnokov@pec.org.ru>
2022-10-12 10:29:41 +0300

    fix PEP 8

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+
 
 class User(AbstractUser):
     USER = 'user'
