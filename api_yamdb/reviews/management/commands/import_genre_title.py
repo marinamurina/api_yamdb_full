7b5b8fd6cb7d0705342fdda2f942d72ac0f29a23
Marina Murina <107209716+marinamurina@users.noreply.github.com>
2023-01-07 20:08:55 +0300

    Update import_genre_title.py

--- api_yamdb/reviews/management/commands/import_genre_title.py
+++ api_yamdb/reviews/management/commands/import_genre_title.py
@@ -1,5 +1,6 @@
 import csv
 
+from django.db import IntegrityError
 from django.core.management.base import BaseCommand
 from reviews.models import Genres, Title, TitleGenres
 
@@ -18,36 +19,36 @@ class Command(BaseCommand):
             dataReader = csv.DictReader(csv_file)
 
             for row in dataReader:
-                genre = Genres()
-                title = Title()
-                title_genre = TitleGenres()
-                title_genre.id = row['id']
-                title.id = row['title_id']
-                genre.id = row['genre_id']
 
-                if TitleGenres.objects.filter(id=title_genre.id).exists():
-                    self.stdout.write(
-                        f'Связь {title.name} - {genre.name}'
-                        f'уже существует в базе.'
+                try:
+                    title_genre_id = row['id']
+                    title_id = row['title_id']
+                    genre_id = row['genre_id']
+                    title = Title.objects.get(id=title_id)
+                    genre = Genres.objects.get(id=genre_id)
+
+                    TitleGenres.objects.create(
+                        id=title_genre_id,
+                        title=title,
+                        genre=genre,
                     )
 
-                elif TitleGenres.objects.filter(
-                    title=title.id, genre=genre.id
-                ).exists():
+                except IntegrityError as err:
                     self.stdout.write(
-                        f'Связь {title.name} - {genre.name}'
-                        f'уже существует в базе.'
+                        f'Связь "{title.name}" '
+                        f'c "{genre.name}" уже внесена в базу. '
+                        f'Ошибка внесения - {err}'
+                    )
+
+                except (Title.DoesNotExist, Genres.DoesNotExist):
+                    self.stdout.write(
+                        f'Произведения с id {title_id} '
+                        f'или жанра с id {genre_id} нет в базе. '
+                        f'Необходимо сначала добавить их в базу.'
                     )
 
                 else:
-                    genre = Genres()
-                    title = Title()
-                    title_genre = TitleGenres()
-                    title_genre.id = row['id']
-                    title.id = row['title_id']
-                    genre.id = row['genre_id']
-                    genre.save()
-
                     self.stdout.write(
-                        f'Связь "{title.name} - {genre.name}" внесена в базу.'
+                        f'Связь "{title.name}" '
+                        f'c "{genre.name}" внесена в базу.'
                     )
